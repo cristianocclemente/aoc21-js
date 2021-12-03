f2bb8b6e9cbb41a72f3be8b1f9ffb30c3c0d097a
cristiano-clemente <cristiano.clemente@tecnico.ulisboa.pt>
2021-12-03 15:42:56 +0000

    🚚 Change project structure

--- day1/part2.js
+++ day1/part2.js
@@ -10,8 +10,10 @@ let increases = 0;
 let index1 = 0;
 let index2 = 1;
 while (index2 + 2 <= numbers.length - 1) {
-  const firstWindow = numbers[index1] + numbers[index1 + 1] + numbers[index1 + 2];
-  const secondWindow = numbers[index2] + numbers[index2 + 1] + numbers[index2 + 2];
+  const firstWindow =
+    numbers[index1] + numbers[index1 + 1] + numbers[index1 + 2];
+  const secondWindow =
+    numbers[index2] + numbers[index2 + 1] + numbers[index2 + 2];
   if (secondWindow > firstWindow) increases++;
   index1++;
   index2++;
