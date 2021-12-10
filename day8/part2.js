e9f35b1d705b61c0a25c1c472d385490a5b06c5c
cristiano-clemente <cristiano.clemente@tecnico.ulisboa.pt>
2021-12-10 00:19:54 +0000

    🎄 Day 8 (part 2)

--- day8/part2.js
+++ day8/part2.js
@@ -11,23 +11,131 @@ const parseInput = (filename) => {
   return entries;
 };
 
-const getTranslateFunction = (signalPatterns) => {
-  const translateFunction = undefined;
-  // TODO: implement this function
-  return translateFunction;
+const digitStringToDigit = (digitString) => {
+  const sortedDigitString = [...digitString].sort().join("");
+
+  const sortedDigitStringToDigit = {
+    abcefg: 0,
+    cf: 1,
+    acdeg: 2,
+    acdfg: 3,
+    bcdf: 4,
+    abdfg: 5,
+    abdefg: 6,
+    acf: 7,
+    abcdefg: 8,
+    abcdfg: 9,
+  };
+
+  return sortedDigitStringToDigit[sortedDigitString];
 };
 
-const getOutputValue = (entry) => {
-  const [signalPatterns, outputValues] = entry;
+const getPermutations = (array) => {
+  const permutations = [];
 
-  const translateFunction = getTranslateFunction(signalPatterns);
-  const translatedOutputValues = translateFunction(outputValues);
+  if (array.length === 0) return [];
 
-  return translatedOutputValues;
+  if (array.length === 1) return [array];
+
+  for (let i = 0; i < array.length; i++) {
+    const currentElement = array[i];
+    const remainingElements = array.slice(0, i).concat(array.slice(i + 1));
+    const remainingElementsPermuted = getPermutations(remainingElements);
+    for (let j = 0; j < remainingElementsPermuted.length; j++) {
+      const permutedArray = [currentElement].concat(
+        remainingElementsPermuted[j]
+      );
+      permutations.push(permutedArray);
+    }
+  }
+  return permutations;
+};
+
+const getWrongSegmentStringToRightSegmentString = (wrongDigitsString) => {
+  // generate all possible translation object permutations
+  const segments = ["a", "b", "c", "d", "e", "f", "g"];
+  const segmentsPermutations = getPermutations(segments);
+
+  const translationObjects = segmentsPermutations.map(
+    (segmentsPermutation) => ({
+      a: segmentsPermutation[0],
+      b: segmentsPermutation[1],
+      c: segmentsPermutation[2],
+      d: segmentsPermutation[3],
+      e: segmentsPermutation[4],
+      f: segmentsPermutation[5],
+      g: segmentsPermutation[6],
+    })
+  );
+
+  // try translation object until the right one is found
+  const originalDigitStrings = [
+    "abcefg",
+    "cf",
+    "acdeg",
+    "acdfg",
+    "bcdf",
+    "abdfg",
+    "abdefg",
+    "acf",
+    "abcdefg",
+    "abcdfg",
+  ];
+
+  for (const translationObject of translationObjects) {
+    const translatedOriginalDigitsString = originalDigitStrings.map(
+      (originalDigitString) =>
+        [...originalDigitString].map((char) => translationObject[char])
+    );
+
+    const sortedTranslatedOriginalDigitsString =
+      translatedOriginalDigitsString.map((translatedOriginalDigitString) =>
+        [...translatedOriginalDigitString].sort().join("")
+      );
+
+    const sortedWrongDigitsString = wrongDigitsString.map((wrongDigitString) =>
+      [...wrongDigitString].sort().join("")
+    );
+
+    if (
+      _.isEqual(
+        sortedTranslatedOriginalDigitsString.sort(),
+        sortedWrongDigitsString.sort()
+      )
+    ) {
+      const invertedTranslationObject = _.invert(translationObject);
+      return invertedTranslationObject;
+    }
+  }
+};
+
+const getRightDisplay = (line) => {
+  const [wrongDigitsString, wrongDisplayString] = line;
+
+  const wrongSegmentStringToRightSegmentString =
+    getWrongSegmentStringToRightSegmentString(wrongDigitsString);
+
+  const rightDisplayString = wrongDisplayString.map((wrongDisplayDigitString) =>
+    [...wrongDisplayDigitString]
+      .map(
+        (wrongDisplaySegmentString) =>
+          wrongSegmentStringToRightSegmentString[wrongDisplaySegmentString]
+      )
+      .join("")
+  );
+
+  const rightDisplay = parseInt(
+    rightDisplayString
+      .map((rightDigitString) =>
+        digitStringToDigit(rightDigitString).toString()
+      )
+      .join("")
+  );
+  return rightDisplay;
 };
 
 const filename = process.argv.slice(2)[0];
-const entries = parseInput(filename);
-const outputValues = entries.map((entry) => getOutputValue(entry));
-const sumOutputValues = _.sum(outputValues);
-console.log(sumOutputValues);
+const lines = parseInput(filename);
+const rightDisplays = lines.map((line) => getRightDisplay(line));
+const sumRightDisplays = _.sum(rightDisplays);
+console.log(sumRightDisplays);
